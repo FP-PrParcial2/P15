4eff4acbf404879ab1961f7128933344edc086d9
karen <karen@KarenZ>
2016-10-21 00:01:57 -0500

    Proyecto parcial

--- src/ppproblema015/PPProblema015.java
+++ src/ppproblema015/PPProblema015.java
@@ -15,22 +15,34 @@ public class PPProblema015 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     int x[][];
-       x=new int [10][10];
-       Random random;
-       random= new Random();
-       
-        for(int i=0; i< x.length; i++){
-            for(int j=0; j< x.length; j++)
-                x[i][j]=random.nextInt(); 
-            int j=1;
-             
-            System.out.println(x[i][j] + "");  
+      int rows = 11;
+        int cols = 11;
+        int[][] arr = new int[rows][cols];
+        Random r = new Random();
+        for(int i = 0; i < rows - 1; i++){
+            int row = 0;
+            int col = 0;
+            for(int j = 0; j < cols - 1; j++){
+                arr[i][j] = r.nextInt(10);
+                arr[j][i] = r.nextInt(10);
+                row += arr[i][j];
+                col += arr[j][i];
+            }
+            arr[i][cols - 1] = row;
+            arr[cols - 1][i] = col;
+        }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
         }
         
-    }
-}
         
+        
+    }
+    
+} 
         
     
   
